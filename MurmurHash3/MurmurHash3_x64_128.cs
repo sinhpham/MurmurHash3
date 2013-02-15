b495591a9ad4e246722f205ccd5d8a8ae4c88682
Sinh Pham <phamansinh@gmail.com>
2013-02-15 11:12:02 -0600

    Clean up & verification test.

--- MurmurHash3/MurmurHash3_x64_128.cs
+++ MurmurHash3/MurmurHash3_x64_128.cs
@@ -13,9 +13,10 @@ namespace MurmurHash3
         {
         }
 
-        UInt64 seed = 0;
-        UInt64 h1 = 0;
-        UInt64 h2 = 0;
+        public UInt64 Seed { get; set; }
+
+        private UInt64 _h1 = 0;
+        private UInt64 _h2 = 0;
 
         protected override void HashCore(byte[] array, int ibStart, int cbSize)
         {
@@ -23,8 +24,8 @@ namespace MurmurHash3
             var len = cbSize;
             int nblocks = len / 16;
 
-            h1 = seed;
-            h2 = seed;
+            _h1 = Seed;
+            _h2 = Seed;
 
             const UInt64 c1 = 0x87c37b91114253d5;
             const UInt64 c2 = 0x4cf5ad432745937f;
@@ -37,23 +38,22 @@ namespace MurmurHash3
 
             for (int i = 0; i < nblocks; i++)
             {
-                k1 = BitConverter.ToUInt64(data, i * 16);//GetBlock(blocks,i*2+0);
-                k2 = BitConverter.ToUInt64(data, i * 16 + 8);//GetBlock(blocks,i*2+1);
+                k1 = BitConverter.ToUInt64(data, i * 16);
+                k2 = BitConverter.ToUInt64(data, i * 16 + 8);
 
-                k1 *= c1; k1 = RotateLeft64(k1, 31); k1 *= c2; h1 ^= k1;
+                k1 *= c1; k1 = RotateLeft64(k1, 31); k1 *= c2; _h1 ^= k1;
 
-                h1 = RotateLeft64(h1, 27); h1 += h2; h1 = h1 * 5 + 0x52dce729;
+                _h1 = RotateLeft64(_h1, 27); _h1 += _h2; _h1 = _h1 * 5 + 0x52dce729;
 
-                k2 *= c2; k2 = RotateLeft64(k2, 33); k2 *= c1; h2 ^= k2;
+                k2 *= c2; k2 = RotateLeft64(k2, 33); k2 *= c1; _h2 ^= k2;
 
-                h2 = RotateLeft64(h2, 31); h2 += h1; h2 = h2 * 5 + 0x38495ab5;
+                _h2 = RotateLeft64(_h2, 31); _h2 += _h1; _h2 = _h2 * 5 + 0x38495ab5;
             }
 
             //----------
             // tail
 
             var tailIdx = nblocks * 16;
-            //const UInt8 * tail = (const UInt8*)(data + nblocks*16);
             k1 = 0;
             k2 = 0;
 
@@ -73,7 +73,7 @@ namespace MurmurHash3
                 case 10: k2 ^= (UInt64)(data[tailIdx + 9]) << 8;
                     goto case 9;
                 case 9: k2 ^= (UInt64)(data[tailIdx + 8]) << 0;
-                    k2 *= c2; k2 = RotateLeft64(k2, 33); k2 *= c1; h2 ^= k2;
+                    k2 *= c2; k2 = RotateLeft64(k2, 33); k2 *= c1; _h2 ^= k2;
                     goto case 8;
                 case 8: k1 ^= (UInt64)(data[tailIdx + 7]) << 56;
                     goto case 7;
@@ -90,29 +90,29 @@ namespace MurmurHash3
                 case 2: k1 ^= (UInt64)(data[tailIdx + 1]) << 8;
                     goto case 1;
                 case 1: k1 ^= (UInt64)(data[tailIdx + 0]) << 0;
-                    k1 *= c1; k1 = RotateLeft64(k1, 31); k1 *= c2; h1 ^= k1;
+                    k1 *= c1; k1 = RotateLeft64(k1, 31); k1 *= c2; _h1 ^= k1;
                     break;
             };
 
             //----------
             // finalization
-            h1 ^= (UInt64)len; h2 ^= (UInt64)len;
+            _h1 ^= (UInt64)len; _h2 ^= (UInt64)len;
 
-            h1 += h2;
-            h2 += h1;
+            _h1 += _h2;
+            _h2 += _h1;
 
-            h1 = FMix(h1);
-            h2 = FMix(h2);
+            _h1 = FMix(_h1);
+            _h2 = FMix(_h2);
 
-            h1 += h2;
-            h2 += h1;
+            _h1 += _h2;
+            _h2 += _h1;
         }
 
         protected override byte[] HashFinal()
         {
             var HashValue = new byte[16];
-            Array.Copy(BitConverter.GetBytes(h1), 0, HashValue, 0, 8);
-            Array.Copy(BitConverter.GetBytes(h2), 0, HashValue, 8, 8);
+            Array.Copy(BitConverter.GetBytes(_h1), 0, HashValue, 0, 8);
+            Array.Copy(BitConverter.GetBytes(_h2), 0, HashValue, 8, 8);
             return HashValue;
         }
 
@@ -129,8 +129,7 @@ namespace MurmurHash3
 
         private UInt64 RotateLeft64(UInt64 x, byte r)
         {
-            return (x << r | x >> (64 - (r & 63)));
-            //return (x << r) | (x >> (64 - r));
+            return (x << r) | (x >> (64 - r));
         }
 
         public override int HashSize
--- MurmurHash3/Program.cs
+++ MurmurHash3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,18 +14,52 @@ namespace MurmurHash3
         {
             var h = new MurmurHash3_x64_128();
 
-            var s = "The quick brown fox jumps over the lazy cog";
-            using (var stream = new MemoryStream())
+            var x = VerificationTest(h, 128, 0x6384BA69, true);
+        }
+
+        private static bool VerificationTest(MurmurHash3_x64_128 hash, int hashbits, UInt32 expected, bool verbose)
+        {
+            int hashbytes = hashbits / 8;
+
+            byte[] key = new byte[256];
+            byte[] hashes = new byte[hashbytes * 256];
+
+            // Hash keys of the form {0}, {0,1}, {0,1,2}... up to N=255,using 256-N as
+            // the seed
+            for (int i = 0; i < 256; i++)
             {
-                using (var writer = new StreamWriter(stream))
+                key[i] = (byte)i;
+                hash.Seed = (ulong)(256 - i);
+                var ret = hash.ComputeHash(key, 0, i);
+                Array.Copy(ret, 0, hashes, i * hashbytes, hashbytes);
+            }
+
+            // Then hash the result array
+            hash.Seed = 0;
+            var final = hash.ComputeHash(hashes, 0, hashbytes * 256);
+
+            // The first four bytes of that hash, interpreted as a little-endian integer, is our
+            // verification value
+
+            UInt32 verification = (UInt32)((final[0] << 0) | (final[1] << 8) | (final[2] << 16) | (final[3] << 24));
+
+            //----------
+
+            if (expected != verification)
+            {
+                if (verbose)
                 {
-                    writer.Write(s);
-                    writer.Flush();
-                    stream.Position = 0;
-                    var x = h.ComputeHash(stream);
-                    var str = BitConverter.ToString(x);
-                    Console.WriteLine(str);
+                    Console.WriteLine("Verification value {0} : Failed! (Expected {1})", verification, expected);
                 }
+                return false;
+            }
+            else
+            {
+                if (verbose)
+                {
+                    Console.WriteLine("Verification value {0} : Passed!", verification);
+                }
+                return true;
             }
         }
     }
